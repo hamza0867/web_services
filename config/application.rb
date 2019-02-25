f223294868c9cda2ca7e1aa3256fa8cfcb340507
hamza0867 <hamza0867@gmail.com>
2019-02-25 10:03:56 +0100

    First commit, pass Custom Types 01, added repo

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require File.expand_path('boot', __dir__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Triresults
+  class Application < Rails::Application
+    Mongoid.load!('./config/mongoid.yml')
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
