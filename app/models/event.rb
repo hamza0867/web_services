2aef4b1cea4d91cb29553d87aa04296688e1fb07
hamza0867 <hamza0867@gmail.com>
2019-02-25 13:21:11 +0100

    Pass Race Collection 02

---
+++ app/models/event.rb
@@ -0,0 +1,25 @@
+class Event
+  include Mongoid::Document
+  field :o, as: :order, type: Integer
+  field :n, as: :name, type: String
+  field :d, as: :distance, type: Float
+  field :u, as: :units, type: String
+
+  def meters
+    case units
+    when 'meters' then distance
+    when 'kilometers' then distance * 1000
+    when 'yards' then distance * 0.9144
+    when 'miles' then distance * 1609.344
+    end
+  end
+
+  def miles
+    case units
+    when 'meters' then distance * 0.000621371
+    when 'kilometers' then distance * 0.621371
+    when 'yards' then distance * 0.000568182
+    when 'miles' then distance
+    end
+  end
+end
