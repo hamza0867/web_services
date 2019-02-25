66216348ba3690e1c8d827baa8192cca81e14b7c
hamza0867 <hamza0867@gmail.com>
2019-02-25 12:23:53 +0100

    Pass Racer Collection 04

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -5,7 +5,11 @@ class RacerInfo
   field :fn, as: :first_name, type: String
   field :ln, as: :last_name,  type: String
   field :g, as: :gender, type: String
-  field :yr, as: :year, type: Integer
+  field :yr, as: :birth_year, type: Integer
   field :res, as: :residence, type: Address
   embedded_in :parent, polymorphic: true
+  validates_presence_of :first_name, :last_name, :birth_year, :gender
+  validates_inclusion_of :gender, in: %w[M F], message: 'must be M or F'
+  validates_numericality_of :birth_year, less_than: Date.today.year,\
+                                         message: 'must in past'
 end
