b747a9b2530e4ee4a41b98e3012a596591fb6b8c
hamza0867 <hamza0867@gmail.com>
2019-02-28 15:04:44 +0100

    Pass service_facade 01

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,6 +4,7 @@ class Race
   field :n, as: :name, type: String
   field :date, type: Date
   field :loc, as: :location, type: Address
+  field :next_bib, type: Integer, default: 0
   embeds_many :events, as: :parent, order: [:order.asc]
   has_many :entrants, foreign_key: :"race._id", dependent: :delete, order: [:secs.asc, :bib.asc]
   scope :upcoming, -> { where(:date.gte => Date.current) }
@@ -49,5 +50,10 @@ class Race
       object.send("#{action}=", name)
       self.location = object
     end
+  end
+
+  def next_bib
+    inc(next_bib: 1)
+    self[:next_bib]
   end
 end
