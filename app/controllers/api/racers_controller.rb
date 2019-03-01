0a7fa47b47d19fee9e746f0be04f13be54361757
hamza0867 <hamza0867@gmail.com>
2019-03-01 12:33:39 +0100

    Pass uri 02

---
+++ app/controllers/api/racers_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  class RacersController < ApplicationController
+    def index; end
+
+    def show; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  # RMM level 2 API routes
+  namespace 'api', as: 'api' do
+    # Races routes
+    get 'races' => 'races#index'
+    get 'races/:id' => 'races#show'
+    get 'races/:id/results' => 'races#results'
+    get 'races/:race_id/results/:id' => 'races#results_detail', :as => 'race_result'
+
+    # Racers routes
+    get 'racers' => 'racers#index'
+    get 'racers/:id' => 'racers#show', :as => 'racer'
+    get 'racers/:racer_id/entries' => 'racers#entries'
+    get 'racers/:racer_id/entries/:id' => 'racers#entries_detail'
+  end
+  # End of RMM level 2 API routes
+
   resources :racers do
     post 'entries' => 'racers#create_entry'
   end
