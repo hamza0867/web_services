46c645337eb1c251fc1737a317abb07009b0669f
hamza0867 <hamza0867@gmail.com>
2019-03-01 12:56:40 +0100

    Pass uri 03

--- app/controllers/api/racers_controller.rb
+++ app/controllers/api/racers_controller.rb
@@ -1,7 +1,36 @@
 module Api
+  # Racers Controller of the API
   class RacersController < ApplicationController
-    def index; end
+    def index
+      if !request.accept || request.accept == '*/*'
+        render plain: '/api/racers'
+      else
+        # real implementation ...
+      end
+    end
 
-    def show; end
+    def show
+      if !request.accept || request.accept == '*/*'
+        render plain: "/api/racers/#{params[:id]}"
+      else
+        # real implementation ...
+      end
+    end
+
+    def entries
+      if !request.accept || request.accept == '*/*'
+        render plain: "/api/racers/#{params[:racer_id]}/entries"
+      else
+        # real implementation ...
+      end
+    end
+
+    def entries_detail
+      if !request.accept || request.accept == '*/*'
+        render plain: "/api/racers/#{params[:racer_id]}/entries/#{params[:id]}"
+      else
+        # real implementation ...
+      end
+    end
   end
 end
--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -1,7 +1,36 @@
 module Api
+  # Races Controller of the API
   class RacesController < ApplicationController
-    def index; end
+    def index
+      if !request.accept || request.accept == '*/*'
+        render plain: '/api/races'
+      else
+        # real implementation ...
+      end
+    end
 
-    def show; end
+    def show
+      if !request.accept || request.accept == '*/*'
+        render plain: "/api/races/#{params[:id]}"
+      else
+        # real implementation ...
+      end
+    end
+
+    def results
+      if !request.accept || request.accept == '*/*'
+        render plain: "/api/races/#{params[:id]}/results"
+      else
+        # real implementation ...
+      end
+    end
+
+    def results_detail
+      if !request.accept || request.accept == '*/*'
+        render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
+      else
+        # real implementation ...
+      end
+    end
   end
 end
